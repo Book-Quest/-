4a8b50bf4afa7a5426ec21b615043565c4e8df1c
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-09-20 20:49:29 +0900

    Create [baekjoon] 나무 자르기.java

---
+++ eunnyeong/[baekjoon] 나무 자르기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static int n, m;
+  static int[] a;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+
+    a = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++)
+      a[i] = Integer.parseInt(st.nextToken());
+
+    Arrays.sort(a);
+
+    int low = 0, high = a[n - 1];
+    while (low < high) {
+      int mid = (low + high) / 2;
+
+      long sum = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        if (a[i] > mid)
+          sum += a[i] - mid;
+        else break;
+      }
+
+      if (sum < m)
+        high = mid;
+      else
+        low = mid + 1;
+    }
+
+    System.out.print(low - 1);
+  }
+}
