6a915e3f2b386933cf6be166ee44b06d3da452aa
Yujeong Song <syj0002012@naver.com>
2024-10-28 16:42:22 +0900

    Update and rename yujeong/[String Matching] 최장 공통 부분 수열.py to yujeong/dp/[String Matching] 최장 공통 부분 수열.py

--- yujeong/dp/[String Matching] 최장 공통 부분 수열.py
+++ yujeong/dp/[String Matching] 최장 공통 부분 수열.py
@@ -7,24 +7,17 @@ b = list(map(str,sys.stdin.readline().rstrip()))
 dp = [[0]*len(b) for _ in range(len(a))]
 
 # 초기값 계산
-if a[0]==b[0]:
-    dp[0][0] = 1
-    for i in range(1,len(a)):
+if a[0]==b[0]: dp[0][0]=1
+for i in range(1,len(a)):
+    if a[i]==b[0]:
         dp[i][0] = 1
-    for j in range(1,len(b)):
+    else:
+        dp[i][0] = dp[i-1][0]
+for j in range(1,len(b)):
+    if a[0]==b[j]:
         dp[0][j] = 1
-else:
-    dp[0][0] = 0
-    same = False
-    for i in range(1,len(a)):
-        if a[i] == b[0] or same:
-            dp[i][0] = 1
-            same = True
-    same = False
-    for j in range(1,len(b)):
-        if a[0] == b[j] or same:
-            dp[0][j] = 1
-            same = True
+    else:
+        dp[0][j] = dp[0][j-1]
 
             
 # 나머지 계산
